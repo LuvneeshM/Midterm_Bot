c10bd523ec5e35efa59fbe5e4de7f4580ad3ff0d
LuvneeshM <lm3300@nyu.edu>
2019-03-08 00:35:12 -0500

    added in the teaching functionality

--- app.js
+++ app.js
@@ -10,15 +10,17 @@ const OPTION_GREET = "greet"
 const OPTION_HELP = "help"
 const OPTION_IMAGE = "image"
 const OPTION_TEACH = "teach"
-// var fuzzyset_commands = FuzzySet(["image", "help", "greeting", "random"])
-// var fuzzyset_images = FuzzySet(["image", "images", "picture", "pic"])
-// var fuzzyset_help = FuzzySet(["help", "assist", "aid", "helpme", "advice", "guide"])
-// var fuzzyset_greet = FuzzySet(["hello", "hi", "yo", "yoo", "good", "morning", "whatsup", "hey"])
+const OPTION_BROKEN = "broken"
+const LEARNING_CATS = ["teaching", "images", "helping", "greeting"]
+
+const GREETINGS = ["Hello", "Good Day", "Today is a new day. Hello", "It is morning somewhere in this world so Morning", "I see you mentioning me in your tweet to greet me"]
 
 class FuzzySet_obj{
-	constructor(fuzzyset, key){
+	constructor(fuzzyset, key, words, fn){
 		this.fuzzyset = fuzzyset
 		this.key = key
+		this.list_words = new Set(words)
+		this.filename = fn
 	}
 	print(){
 		console.log("This is the fuzzyset for " + this.key)
@@ -28,6 +30,10 @@ var fuzzyset_teach = FuzzySet()
 var fuzzyset_images = FuzzySet()
 var fuzzyset_help = FuzzySet()
 var fuzzyset_greet = FuzzySet()
+var fuzzyset_learning_cats = FuzzySet()
+for (var i = 0; i < LEARNING_CATS.length; i++){
+	fuzzyset_learning_cats.add(LEARNING_CATS[i])
+}
 var fuzzyset_list = []
 
 
@@ -39,14 +45,15 @@ function fillFuzzyVars(fuzzyset, filepath){
 	for (let i = 0; i < contents.length; i++){
 		fuzzyset.add(contents[i])
 	}
+	return contents
 }
 
 function setup(){
-	fillFuzzyVars(fuzzyset_teach, "learning.txt")
-	fillFuzzyVars(fuzzyset_images, "images.txt")
-	fillFuzzyVars(fuzzyset_help, "help.txt")
-	fillFuzzyVars(fuzzyset_greet, "greetings.txt")
-	fuzzyset_list = [new FuzzySet_obj(fuzzyset_teach, OPTION_TEACH), new FuzzySet_obj(fuzzyset_images, OPTION_IMAGE), new FuzzySet_obj(fuzzyset_help, OPTION_HELP), new FuzzySet_obj(fuzzyset_greet, OPTION_GREET)]
+	let tc = fillFuzzyVars(fuzzyset_teach, "learning.txt")
+	let ic = fillFuzzyVars(fuzzyset_images, "images.txt")
+	let hc = fillFuzzyVars(fuzzyset_help, "help.txt")
+	let gc = fillFuzzyVars(fuzzyset_greet, "greetings.txt")
+	fuzzyset_list = [new FuzzySet_obj(fuzzyset_teach, OPTION_TEACH, tc, "learning.txt"), new FuzzySet_obj(fuzzyset_images, OPTION_IMAGE, ic, "images.txt"), new FuzzySet_obj(fuzzyset_help, OPTION_HELP, hc, "help.txt"), new FuzzySet_obj(fuzzyset_greet, OPTION_GREET, gc, "greetings.txt")]
 }
 
 /*********
@@ -88,12 +95,65 @@ function analyzeTweet(text) {
 	//compare with the different fuzzy sets and choose the closest if possible
 	let fuzzy_to_use = compareAgainstFuzzies(command)
 	if (fuzzy_to_use == null){
-		console.log("1. send back message saying you do not understand what is being said")
+		return OPTION_BROKEN
 	} else {
 		return fuzzy_to_use.key
 	}
 }
 
+function tweetBackToUser_text(params){
+	T.post("statuses/update", params, function(err, data, response){
+		if(err){
+			console.log("Error in tweeting back to user text")
+		}
+		else{
+			console.log("Tweeted " + params.status)
+		}
+	})
+}
+
+function writeToFile(list_of_words, fn){
+	const filedata = Array.from(list_of_words).join("\n")
+	fs.writeFileSync(fn, filedata)
+}
+
+//3: greeting
+//2: helping
+//1: images
+//0: teaching
+function getSmarter(text){
+	let text_nospace = text.split(" ").join("")
+	let tokens = text_nospace.split(":")
+	let cat = tokens[1]
+	let new_command = tokens[2]
+	let score_command_list = fuzzyset_learning_cats.get(cat)
+	if (score_command_list == null){
+		return false
+	}
+
+	let chosen_cat = score_command_list[0][1]
+	console.log(chosen_cat)
+	if (chosen_cat == LEARNING_CATS[0]){
+		fuzzyset_teach.add(new_command)
+		fuzzyset_list[0].list_words.add(new_command)
+		writeToFile(fuzzyset_list[0].list_words, fuzzyset_list[0].filename)
+	} else if (chosen_cat == LEARNING_CATS[1]){
+		fuzzyset_images.add(new_command)
+		fuzzyset_list[1].list_words.add(new_command)
+		writeToFile(fuzzyset_list[1].list_words, fuzzyset_list[1].filename)
+	}else if (chosen_cat == LEARNING_CATS[2]){
+		fuzzyset_help.add(new_command)
+		fuzzyset_list[2].list_words.add(new_command)
+		writeToFile(fuzzyset_list[2].list_words, fuzzyset_list[2].filename)
+	}else if (chosen_cat == LEARNING_CATS[3]){
+		fuzzyset_greet.add(new_command)
+		fuzzyset_list[3].list_words.add(new_command)
+		writeToFile(fuzzyset_list[3].list_words, fuzzyset_list[3].filename)
+	}
+
+
+	return true
+}
 
 /*************
 * Part 1
@@ -120,9 +180,15 @@ function updateLastTweetFile(most_recent_tweet_text) {
 	fs.closeSync(fd)
 }
 
-function tweetBack(err, data, response) {
+//for the delay
+function sleep(ms){
+	return new Promise(resolve=>{
+		setTimeout(resolve,ms)
+	})
+}
+async function tweetBack(err, data, response) {
 	if (err){
-		console.log("err")
+		console.log("err in tweetBack")
 		console.log(err)
 	} else {
 		//first we need to figure out what the last tweet we responded to was
@@ -133,7 +199,6 @@ function tweetBack(err, data, response) {
 		while(i < data.length){
 			var tweet_obj = data[i]
 			var tweet_text = tweet_obj["text"].toLowerCase()
-			console.log(tweet_text)
 			//we have seen this tweet before
 			//that means we have seen all the tweets that follow in data
 			if (tweet_text == last_tweet){
@@ -145,16 +210,63 @@ function tweetBack(err, data, response) {
 			response_option = analyzeTweet(tweet_text)
 			if (response_option == OPTION_TEACH){
 				console.log("time to get smarter")
+				let success = getSmarter(tweet_text)
+				if (success){
+					//DONE
+					let reply = "Thank you @" + tweet_obj["user"]["screen_name"] + "! Your contribution has helped me get smarter!"
+					let params = {
+						status: reply, 
+						in_reply_to_status_id: tweet_obj.id_str
+					}
+					console.log("reply learned something new")
+					tweetBackToUser_text(params)
+				} else{
+					//DONE
+					let reply = "Thank you for trying to help me learn, but it seems i did not understand you, @" + tweet_obj["user"]["screen_name"]
+					let params = {
+						status: reply, 
+						in_reply_to_status_id: tweet_obj.id_str
+					}
+					console.log("failed to learned something new")
+					tweetBackToUser_text(params)
+				}
+
 			} else if (response_option == OPTION_IMAGE){
+				//TODO
 				console.log("google search image and send image")
+
+
+
 			}
 			else if (response_option == OPTION_HELP){
+				//TODO
 				console.log("tell user how to use me")
+
+
+
+
 			} else if (response_option == OPTION_GREET){
-				console.log("I will tweet back at the user now")
+				//DONE
+				let reply = GREETINGS[Math.floor(Math.random()*GREETINGS.length)] + " @" + tweet_obj["user"]["screen_name"] + "!" 
+				let params = {
+					status: reply, 
+					in_reply_to_status_id: tweet_obj.id_str
+				}
+				console.log("replying greet")
+				tweetBackToUser_text(params)
 			} else {
-				console.log("Tweet back saying sorry I dont understand you and say you can try again with another @ mention thing")
+				//DONE
+				let reply = "Tweet back to " + " @"+ tweet_obj["user"]["screen_name"] + " Sorry I dont understand you. You can try again with another @ mention and ask for help. Example: <Mention> help"
+				let params = {
+					status: reply, 
+					in_reply_to_status_id: tweet_obj.id_str
+				}
+				console.log("user makes no sense, replying help")
+				tweetBackToUser_text(params)
 			}
+
+			//small delay between tweets bot catches up to
+			await sleep(1)
 
 			i += 1
 		}
@@ -197,7 +309,7 @@ function main(){
 	//set up
 	setup()
 	//part 1
-	T.get('statuses/mentions_timeline', {count: 2}, tweetBack)
+	T.get('statuses/mentions_timeline', {count: 10}, tweetBack)
 	//part 2
 
 }
